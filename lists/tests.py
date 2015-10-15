e9d034eae63d77eb03f7f5f877857d03acf30fdd
pdyban <pdyban@gmail.com>
2015-10-15 21:34:00 +0200

    Replaced HttpResponse with template rendering in lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from .views import home_page
+from django.template.loader import render_to_string
 
 
 # Create your tests here.
@@ -14,6 +15,5 @@ class SmokeTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('lists/home.html')
+        self.assertTrue(response.content.decode(), expected_html)
