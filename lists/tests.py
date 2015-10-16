d16c931cd5d3a5a25f10731061a10d1b33023f10
pdyban <pdyban@gmail.com>
2015-10-16 18:52:33 +0200

    Implemented handling of POST request in lists/home.html, extended FT to test against multiple inputs

--- lists/tests.py
+++ lists/tests.py
@@ -10,10 +10,26 @@ class SmokeTest(TestCase):
 
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
+
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('lists/home.html')
+
         self.assertTrue(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+
+        expected_html = render_to_string('lists/home.html',
+                                         {'new_item_text': 'A new list item'}
+                                         )
+        self.assertEqual(expected_html, response.content.decode())
