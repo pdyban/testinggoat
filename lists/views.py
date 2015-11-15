09675c2996de22dd1c139c410580bd4a589b9932
pdyban <pdyban@gmail.com>
2015-11-15 23:03:20 +0100

    Created a new view for list display, separate from home_page

--- lists/views.py
+++ lists/views.py
@@ -7,8 +7,12 @@ from .models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
     return render(request, 'lists/home.html', {
         'items': Item.objects.all()
     })
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'lists/home.html', {'items': items})
