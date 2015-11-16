685a1f0f41becd8abdaba9b42668d66fae972ece
pdyban <pdyban@gmail.com>
2015-11-16 20:24:10 +0100

    Created a template for list view

--- lists/views.py
+++ lists/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Item
 
 
-# Create your views here.
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
 
-    return render(request, 'lists/home.html', {
-        'items': Item.objects.all()
-    })
+    return render(request, 'lists/home.html')
+
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'lists/home.html', {'items': items})
+    return render(request, 'lists/list.html', {'items': items})
